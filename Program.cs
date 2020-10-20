c2fac3b73b3b96c82ef880f16d41b7146c566817
Stephen Jones <sjones@gnty.com>
2020-10-20 14:57:48 -0500

    refactor, incorporate right settings

--- Program.cs
+++ Program.cs
@@ -24,14 +24,6 @@ namespace CreateEncryptionKeyFiwire
 
         private static void HandleParsed(CommandLineOptions options)
         {
-            using RijndaelManaged myRijndael = new RijndaelManaged
-            {
-                BlockSize = 128,
-                KeySize = 256,
-                Padding = PaddingMode.PKCS7,
-                Mode = CipherMode.CBC
-            };
-
             byte[] encrypted = EncryptStringToBytes(
                 plainText: GetValueToEncrypt(options.SharedSecret), 
                 Key: Convert.FromBase64String(options.KeyValue), 
@@ -39,6 +31,7 @@ namespace CreateEncryptionKeyFiwire
             );
 
             Console.WriteLine(Convert.ToBase64String(encrypted));
+            Console.ReadKey();
         }
 
         private static void HandleParseError(IEnumerable<Error> errs)
@@ -56,42 +49,30 @@ namespace CreateEncryptionKeyFiwire
 
         private static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
         {
-            // Check arguments.
-            if (plainText == null || plainText.Length <= 0)
-                throw new ArgumentNullException("plainText");
-            if (Key == null || Key.Length <= 0)
-                throw new ArgumentNullException("Key");
-            if (IV == null || IV.Length <= 0)
-                throw new ArgumentNullException("IV");
-            byte[] encrypted;
-            // Create an RijndaelManaged object
-            // with the specified key and IV.
-            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            using RijndaelManaged rijAlg = new RijndaelManaged
             {
-                rijAlg.Key = Key;
-                rijAlg.IV = IV;
+                BlockSize = 128,
+                KeySize = 256,
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+                Key = Key,
+                IV = IV
+            };
 
-                // Create an encryptor to perform the stream transform.
-                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
 
-                // Create the streams used for encryption.
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-
-                            //Write all data to the stream.
-                            swEncrypt.Write(plainText);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
-                }
+            // Create the streams used for encryption.
+            using MemoryStream msEncrypt = new MemoryStream();                
+            using CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+            {
+                //Write all data to the stream.
+                swEncrypt.Write(plainText);
             }
 
             // Return the encrypted bytes from the memory stream.
-            return encrypted;
+            return msEncrypt.ToArray();
         }
     }
 }
